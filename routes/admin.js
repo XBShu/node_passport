5c318ece7c76e3b17173c3bd8286e0f73d9ff76f
Shuxin Xu Bao <sxb204@nyu.edu>
2020-11-15 15:30:11 +0100

    added admin route and views

---
+++ routes/admin.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const {ensureAuthenticated} = require('../config/auth');
+
+
+router.get('/', (req, res) => {
+    res.render("admin");
+})
+
+module.exports = router;
